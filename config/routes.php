a1569de78473e4c24fff99855d15faf3f28240f3
michelint <lemoderateurdelive@gmail.com>
2024-12-03 11:37:15 +0100

    Maj : Rajout de la route pour le futur controlleur

--- config/routes.php
+++ config/routes.php
@@ -6,5 +6,6 @@ return [
     '/sanctions/create' => ['ConnexionController', 'create'],
     '/sanctions/login' => ['ConnexionController', 'login'],
     '/sanctions/logout' => ['ConnexionController', 'logout'],
-    '/legal' => ['MentionLegalController', 'legal']
+    '/legal' => ['MentionLegalController', 'legal'],
+    '/promotions/create' => ['PromotionController', 'create']
 ];
